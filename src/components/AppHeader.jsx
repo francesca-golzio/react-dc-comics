ae9ed0993b32e61eda2a11c48cd8222f70de9959
Francesca Golzio <francescagolzio@gmail.com>
2026-03-06 18:49:18 +0100

    add header elements
    no style

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,9 +1,25 @@
+import dcLogo from '/dc-logo.png'
+
 export default function AppHeader(){
 
   return(
     <>
       <header>
-        Header
+        <div className="container">
+          <img src={dcLogo} alt="" />
+          <nav>
+            <a className='toUpperCase'>characters</a>
+            <a className='toUpperCase onFocus'>comic</a>
+            <a className='toUpperCase'>movies</a>
+            <a className='toUpperCase'>tv</a>
+            <a className='toUpperCase'>games</a>
+            <a className='toUpperCase'>collectibles</a>
+            <a className='toUpperCase'>videos</a>
+            <a className='toUpperCase'>fans</a>
+            <a className='toUpperCase'>news</a>
+            <a className='toUpperCase'>shop</a>
+          </nav>
+        </div>
       </header>
     </>
   )
