7fa842b3016b8c3296104b94c831d2bdeda40322
Francesca Golzio <francescagolzio@gmail.com>
2026-03-06 19:41:10 +0100

    add footer elements
    no style (but yet some style oriented className)

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,9 +1,75 @@
+import dcLogoBig from '/dc-logo-bg.png'
+
 export default function AppFooter() {
 
   return (
     <>
       <footer>
-        Footer
+        <div className="container">
+
+          <div>
+            <nav>
+
+              <h4 className='text-uppercase'>dc comics</h4>
+              <ul>
+                <li>Characters</li>
+                <li>Comic</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+              </ul>
+
+              <h4 className='text-uppercase'>shop</h4>
+              <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+              </ul>
+
+              <h4 className='text-uppercase'>dc</h4>
+              <ul>
+                <li>Terms Of Use</li>
+                <li>Privacy policy (New)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+              </ul>
+
+              <h4 className='text-uppercase'>sites</h4>
+              <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+              </ul>
+
+            </nav>
+            <img src={dcLogoBig} alt="" />
+          </div>
+
+          <div>
+            <button className='text-uppercase'>sign-up now!</button>
+            <div className="followUs">
+              <span className='text-uppercase'>follow us</span>
+              <div>
+                <a href="" className="social">fb</a>
+                <a href="" className="social">tw</a>
+                <a href="" className="social">yt</a>
+                <a href="" className="social">pin</a>
+                <a href="" className="social">map</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </footer>
     </>
   )
