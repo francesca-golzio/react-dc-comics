830a4aba44b0341ba1ce43dd9f052ebf69837d31
Francesca Golzio <francescagolzio@gmail.com>
2026-03-06 18:27:41 +0100

    Add macro components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AppHeader from './components/AppHeader'
+import AppMain from './components/AppMain'
+import AppFooter from './components/AppFooter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
     </>
   )
 }
