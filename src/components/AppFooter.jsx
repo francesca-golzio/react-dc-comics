a5178366985fb9450bdbe926b9e44974f027d049
Francesca Golzio <francescagolzio@gmail.com>
2026-03-07 18:36:39 +0100

    add some styling to footeradd some style to Footer
    also correlated partial revision of  block hierarchy

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -5,57 +5,59 @@ export default function AppFooter() {
   return (
     <>
       <footer>
-        <div className="container">
 
-          <div>
-            <nav>
+        <div>
+          <div className='bg-comics'>
+            <div className="container">
+              <nav>
 
-              <h4 className='text-uppercase'>dc comics</h4>
-              <ul>
-                <li>Characters</li>
-                <li>Comic</li>
-                <li>Movies</li>
-                <li>TV</li>
-                <li>Games</li>
-                <li>Videos</li>
-                <li>News</li>
-              </ul>
+                <h4 className='text-uppercase'>dc comics</h4>
+                <ul>
+                  <li>Characters</li>
+                  <li>Comic</li>
+                  <li>Movies</li>
+                  <li>TV</li>
+                  <li>Games</li>
+                  <li>Videos</li>
+                  <li>News</li>
+                </ul>
 
-              <h4 className='text-uppercase'>shop</h4>
-              <ul>
-                <li>Shop DC</li>
-                <li>Shop DC Collectibles</li>
-              </ul>
+                <h4 className='text-uppercase'>shop</h4>
+                <ul>
+                  <li>Shop DC</li>
+                  <li>Shop DC Collectibles</li>
+                </ul>
 
-              <h4 className='text-uppercase'>dc</h4>
-              <ul>
-                <li>Terms Of Use</li>
-                <li>Privacy policy (New)</li>
-                <li>Ad Choices</li>
-                <li>Advertising</li>
-                <li>Jobs</li>
-                <li>Subscriptions</li>
-                <li>Talent Workshops</li>
-                <li>CPSC Certificates</li>
-                <li>Ratings</li>
-                <li>Shop Help</li>
-                <li>Contact Us</li>
-              </ul>
+                <h4 className='text-uppercase'>dc</h4>
+                <ul>
+                  <li>Terms Of Use</li>
+                  <li>Privacy policy (New)</li>
+                  <li>Ad Choices</li>
+                  <li>Advertising</li>
+                  <li>Jobs</li>
+                  <li>Subscriptions</li>
+                  <li>Talent Workshops</li>
+                  <li>CPSC Certificates</li>
+                  <li>Ratings</li>
+                  <li>Shop Help</li>
+                  <li>Contact Us</li>
+                </ul>
 
-              <h4 className='text-uppercase'>sites</h4>
-              <ul>
-                <li>DC</li>
-                <li>MAD Magazine</li>
-                <li>DC Kids</li>
-                <li>DC Universe</li>
-                <li>DC Power Visa</li>
-              </ul>
+                <h4 className='text-uppercase'>sites</h4>
+                <ul>
+                  <li>DC</li>
+                  <li>MAD Magazine</li>
+                  <li>DC Kids</li>
+                  <li>DC Universe</li>
+                  <li>DC Power Visa</li>
+                </ul>
 
-            </nav>
-            <img src={dcLogoBig} alt="" />
+              </nav>
+              <img src={dcLogoBig} alt="" />
+            </div>
           </div>
 
-          <div>
+          <div className="container footerContainer">
             <button className='text-uppercase'>sign-up now!</button>
             <div className="followUs">
               <span className='text-uppercase'>follow us</span>
