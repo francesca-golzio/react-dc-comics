6e9c5230fb7fbf03186fb91d8a66daa87ccdc0d0
Francesca Golzio <francescagolzio@gmail.com>
2026-03-09 16:57:41 +0100

    add main comics deck
    with sample img and text

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,9 +1,28 @@
+import comics from '../assets/dati/comics'
+
 export default function AppMain() {
 
   return (
     <>
       <main>
-        ➡️ Contenct goes here ⬅️
+        <div className="container">
+          <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 rounded-0 comic_card_deck">
+            {comics.map((item) => {
+              return (
+                <div className="col">
+                  <div className="card-body comic_card_body">
+                    <img src='https://picsum.photos/id/237/200/300' className="card-img-top comic_card_img" />
+                    <div className="col text-uppercase card-title">Title</div>
+                  </div>
+                </div>
+              )
+            }
+            )}
+          </div>
+          <div className='more_comics'>
+            <button className='more_comics_btn'>load more</button>
+          </div>
+        </div>
       </main>
     </>
   )
