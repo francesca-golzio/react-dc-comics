c04894f06511c73d525ce804ab95efdaae510b4e
Francesca Golzio <francescagolzio@gmail.com>
2026-03-09 11:29:54 +0100

    make the navbar dynamic
    whit iteration

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,23 +1,67 @@
 import dcLogo from '/dc-logo.png'
 
-export default function AppHeader(){
+const navMenu = [
+  {
+    id: 1,
+    menuLabel: 'characters',
+    url: '/characters'
+  },
+  {
+    id: 2,
+    menuLabel: 'comic',
+    url: '/comic'
+  },
+  {
+    id: 3,
+    menuLabel: 'movies',
+    url: '/movies'
+  },
+  {
+    id: 4,
+    menuLabel: 'tv',
+    url: '/tv'
+  },
+  {
+    id: 5,
+    menuLabel: 'games',
+    url: '/games'
+  },
+  {
+    id: 6,
+    menuLabel: 'collectibles',
+    url: '/collectibles'
+  },
+  {
+    id: 7,
+    menuLabel: 'videos',
+    url: '/videos'
+  },
+  {
+    id: 8,
+    menuLabel: 'fans',
+    url: '/fans'
+  },
+  {
+    id: 9,
+    menuLabel: 'news',
+    url: '/news'
+  },
+  {
+    id: 10,
+    menuLabel: 'shop',
+    url: '/shop'
+  }
+]
 
-  return(
+export default function AppHeader() {
+
+  return (
     <>
       <header>
         <div className="container d-flex justify-content-between align-items-center">
-          <img src={dcLogo} alt="" />
+          <img src={dcLogo} alt="logo" />
           <nav className='d-flex gap-4'>
-            <a className='text-uppercase'>characters</a>
-            <a className='text-uppercase onFocus'>comic</a>
-            <a className='text-uppercase'>movies</a>
-            <a className='text-uppercase'>tv</a>
-            <a className='text-uppercase'>games</a>
-            <a className='text-uppercase'>collectibles</a>
-            <a className='text-uppercase'>videos</a>
-            <a className='text-uppercase'>fans</a>
-            <a className='text-uppercase'>news</a>
-            <a className='text-uppercase'>shop</a>
+          {navMenu.map(voice => {return (<a href={voice.url} className='text-uppercase' key={voice.id}>{voice.menuLabel}</a>)})}
           </nav>
         </div>
       </header>
